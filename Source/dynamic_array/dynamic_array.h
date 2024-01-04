6d122882a43aa04702e65e8866b1c90eceace621
Alrick Grandison <alrickgrandison@algodal.com>
2024-01-04 00:20:11 -0500

    update doc

--- Source/dynamic_array/dynamic_array.h
+++ Source/dynamic_array/dynamic_array.h
@@ -63,20 +63,38 @@ bool dynamic_array_init(dynamic_array_t* da);
 /// Checks if the dynamic array is empty.
 bool dynamic_array_empty(dynamic_array_t* da);
 
+/// Current length of the dynamic array
 size_t dynamic_array_length(dynamic_array_t* da);
 
+/// Current raw array of the dynamic array
 void* dynamic_array_array(dynamic_array_t* da);
 
+/// Resize the dynamic array to fit the length.
+/// If you call this function, before calling any other function
+/// you should call @ref dynamic_array_init again
 void dynamic_array_fit(dynamic_array_t* da);
 
+/// Get the actual position used to access the item.
+/// @param rel_pos can be positive or negative.
+///                If positive then it is treated as the absolute position
+///                else it is treated as a reverse position.
 size_t dynamic_array_resolve_position(dynamic_array_t* da, long long rel_pos);
 
+/// Get the item at the specified position.
+/// @param ret_item optional pointer to where the item is copied
+/// @return returns the item at the specified position
 void* dynamic_array_get_item(dynamic_array_t* da, long long rel_pos, void* ret_item);
 
+/// Add an item to the end of the dynamic array
+/// @param item item to add
 void dynamic_array_append_item(dynamic_array_t* da, void* item);
 
+/// Insert an item at the specified position of the dynamic array
+/// @param item item to insert
 void dynamic_array_insert_item(dynamic_array_t* da, long long rel_pos, void* item);
 
+/// Delete an item at the specified position of the dynamic array
+/// @param item item to delete
 void* dynamic_array_delete_item(dynamic_array_t* da, long long rel_pos, void* ret_item);
 
 #ifdef __cplusplus
